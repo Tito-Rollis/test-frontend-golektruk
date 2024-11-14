193102bf0289816df525e1217d7ba9777da6bbfc
Tito-Rollis <ouvalres@gmail.com>
2024-11-14 13:06:02 +0700

    Soal 4 berhasil

--- src/soal/soal4.tsx
+++ src/soal/soal4.tsx
@@ -1,55 +1,112 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect, useCallback } from 'react';
 
+interface PokemonDataType {
+    count: 1302;
+    next: 'https://pokeapi.co/api/v2/pokemon/?offset=15&limit=10';
+    previous: 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=5';
+    results: { name: string; url: string }[];
+}
 // Soal Nomor 4
 // Buatlah sebuah website yang menampilkan daftar pokemon yang di load dengan infinite scroll
 
-const BASE_URL = 'https://pokeapi.co/api/v2'
+const BASE_URL = 'https://pokeapi.co/api/v2';
 
-const fetchPokemon = async (offset: number, limit: number) => {
-  // fungsi untuk fetch data pokemon
-}
+const fetchPokemon: (offset: number, limit: number) => Promise<PokemonDataType> = async (
+    offset: number,
+    limit: number
+) => {
+    // fungsi untuk fetch data pokemon
+    const response = await fetch(`${BASE_URL}/pokemon/?offset=${offset}&limit=${limit}`);
+    const pokemon: Promise<PokemonDataType> = response.json();
+    return pokemon;
+};
 
 const Soal4 = () => {
-  const [pokemonList, setPokemonList] = useState<
-    { name: string; url: string }[]
-  >([])
-  //  Tambahkan state yang dibutuhkan
-  // ...
+    const [pokemonList, setPokemonList] = useState<{ name: string; url: string }[]>([]);
+    //  Tambahkan state yang dibutuhkan
+    // ...
+    const [loading, setLoading] = useState(true);
+    const [offset, setOffset] = useState(0);
+    const [limit, setLimit] = useState(50);
 
-  const theRef = useRef<HTMLDivElement | null>(null)
+    console.log(pokemonList);
 
-  // Fungsi untuk infinite scroll
-  // ...
-  return (
-    <div
-      style={{
-        display: 'flex',
-        minHeight: '100vh',
-      }}>
-      <div
-        style={{
-          flexGrow: 1,
-          color: 'white',
-          fontSize: '1.5em',
-          textAlign: 'center',
-          fontFamily: 'sans-serif',
-        }}>
-        <h1
-          style={{
-            fontWeight: 'bolder',
-          }}>
-          Pokémon Infinite Scroll
-        </h1>
-        {/* list pokemon beserta loading */}
-      </div>
-      <iframe
-        src='/soal4.mp4'
-        style={{
-          height: '100vh',
-          border: '1px solid white',
-        }}></iframe>
-    </div>
-  )
-}
+    const theRef = useRef<HTMLDivElement | null>(null);
+    const intObserver = useRef<IntersectionObserver>();
+    const lastPokemon = useCallback((node: HTMLDListElement) => {
+        setLoading(true);
+        if (node === null) return;
+        if (intObserver.current) intObserver.current.disconnect();
+        intObserver.current = new IntersectionObserver((entries) => {
+            if (entries[0].isIntersecting) {
+                handleInfiniteScroll();
+                console.log(entries[0].target.textContent);
+            }
+        });
+        intObserver.current.observe(node);
+    }, []);
+    const handleFetchPokemon = async (offset: number, limit: number) =>
+        fetchPokemon(offset, limit).then((pokemon: PokemonDataType) => {
+            setPokemonList(pokemon.results);
+            setLoading(false);
+        });
 
-export default Soal4
+    useEffect(() => {
+        setLoading(true);
+        handleFetchPokemon(offset, limit);
+    }, [offset, limit]);
+
+    // Fungsi untuk infinite scroll
+    // ...
+    const handleInfiniteScroll = () => {
+        setLoading(true);
+        setOffset((prev) => prev + 50);
+        setLimit((prev) => prev + 50);
+    };
+    return (
+        <div
+            style={{
+                display: 'flex',
+                minHeight: '100vh',
+            }}
+        >
+            <div
+                style={{
+                    flexGrow: 1,
+                    color: 'white',
+                    fontSize: '1.5em',
+                    textAlign: 'center',
+                    fontFamily: 'sans-serif',
+                }}
+            >
+                <h1
+                    style={{
+                        fontWeight: 'bolder',
+                    }}
+                >
+                    Pokémon Infinite Scroll
+                </h1>
+                {/* list pokemon beserta loading */}
+                <ul>
+                    {pokemonList.map((pokemon, id) => (
+                        <li ref={id === pokemonList.length - 1 ? lastPokemon : null} key={id}>
+                            {pokemon.name}
+                        </li>
+                    ))}
+                </ul>
+                {loading && (
+                    <h1 style={{ color: 'white', fontSize: '32px', textAlign: 'center' }}>Loading more Pokémon....</h1>
+                )}
+            </div>
+            <iframe
+                src="/soal4.mp4"
+                style={{
+                    height: '100vh',
+                    border: '1px solid white',
+                }}
+            ></iframe>
+        </div>
+    );
+};
+
+export default Soal4;
--- src/soal/soal5.tsx
+++ src/soal/soal5.tsx
@@ -1,48 +1,59 @@
-import { useState } from "react";
-import ReactDOM from "react-dom";
+import { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
 
 const Soal5 = () => {
-  // 5. buatlah fungsi untuk menutup modal ketika tombol back browser diklik
-  const [openModal, setOpenModal] = useState<boolean>(false);
+    // 5. buatlah fungsi untuk menutup modal ketika tombol back browser diklik
+    const [openModal, setOpenModal] = useState<boolean>(false);
 
-  return (
-    <>
-      <div style={{ margin: "1rem" }}>
-        {openModal && <Modal />}
-        <button
-          style={{ padding: "2px 4px", background: "white" }}
-          onClick={() => {
-            setOpenModal((prev) => !prev);
-          }}
-        >
-          {openModal ? "close" : "open"} modal
-        </button>
-      </div>
+    useEffect(() => {
+        window.history.pushState(null, '');
+        window.addEventListener('popstate', () => {
+            alert('BACK');
+        });
+        return () =>
+            window.removeEventListener('popstate', () => {
+                alert('BACK');
+            });
+    });
 
-      {/* Ekspektasi hasil */}
-      <iframe
-        src="/soal5.mp4"
-        style={{
-          position: "fixed",
-          bottom: 0,
-          right: 0,
-          border: "1px solid white",
-        }}
-      ></iframe>
-    </>
-  );
+    return (
+        <>
+            <div style={{ margin: '1rem' }}>
+                {openModal && <Modal />}
+                <button
+                    style={{ padding: '2px 4px', background: 'white' }}
+                    onClick={() => {
+                        setOpenModal((prev) => !prev);
+                    }}
+                >
+                    {openModal ? 'close' : 'open'} modal
+                </button>
+            </div>
+
+            {/* Ekspektasi hasil */}
+            <iframe
+                src="/soal5.mp4"
+                style={{
+                    position: 'fixed',
+                    bottom: 0,
+                    right: 0,
+                    border: '1px solid white',
+                }}
+            ></iframe>
+        </>
+    );
 };
 
 const Modal = () => {
-  const modalRoot = document.getElementById("modal-root");
+    const modalRoot = document.getElementById('modal-root');
 
-  if (!modalRoot) return <></>;
-  return ReactDOM.createPortal(
-    <section style={{ background: "#8f9cb0", padding: "3rem", position: "fixed", margin: "6rem" }}>
-      <div>This is modal</div>
-    </section>,
-    modalRoot
-  );
+    if (!modalRoot) return <></>;
+    return ReactDOM.createPortal(
+        <section style={{ background: '#8f9cb0', padding: '3rem', position: 'fixed', margin: '6rem' }}>
+            <div>This is modal</div>
+        </section>,
+        modalRoot
+    );
 };
 
 export default Soal5;
